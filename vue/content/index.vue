a5237d5726c2afc45c30fe3071d8edad4f2adbf0
ssbramon <ssbramson@gmail.com>
2019-01-23 11:58:46 -0800

    Fixing minor grammar issues

--- vue/content/index.vue
+++ vue/content/index.vue
@@ -9,7 +9,7 @@ new Vue({
         <h1 class="index-header" aria-label="Spencer Bramson's make up is 60% Koala + 10% Discipline + 20% Nerd + 3% Failing + 7% Coloring outside the lines">🐨<sup>.60</sup> + 🥋<sup>.10</sup> + 🤓<sup>.20</sup> + 🤦‍<sup> .03</sup> + 🤪‍<sup>.07</sup></h1>
         
         <section class="index-introduction_paragraph-content" aria-label="Spencer Bramson's introduction">
-          <p class="index-introduction_paragraph">Award winning marketing executive specialized in consumer technology applications and packaged goods. Clientele including, but not limited to;</p>
+          <p class="index-introduction_paragraph">Award-winning marketing executive specialized in consumer technology applications and packaged goods. Past clientele including, but not limited to;</p>
           
           <ul class="index-highlighted_clients"><li class="index-highlighed_client" v-for="indexClientLink in indexClientLinks"><a class="index-hightlighted_client-link" :href="indexClientLink.linkURL">{{ indexClientLink.linkTitle }}</a></li></ul>
         </section>
