7d4bf13ca719e69c8bfa0a63fd6a06942b78a459
ssbramon <ssbramson@gmail.com>
2019-01-23 19:04:10 -0800

    Testing PWA status

--- vue/content/index.vue
+++ vue/content/index.vue
@@ -6,13 +6,9 @@ new Vue({
 
       <section class="index-content" aria-label="Site" role="contentinfo">
       
-        <h1 class="index-header" aria-label="Spencer Bramson's make up is 60% Koala + 10% Discipline + 20% Nerd + 3% Failing + 7% Coloring outside the lines">🐨<sup>.60</sup> + 🥋<sup>.10</sup> + 🤓<sup>.20</sup> + 🤦‍<sup> .03</sup> + 🤪‍<sup>.07</sup></h1>
+        <h1 class="index-header" aria-label="Spencer Bramson's DNA is 60% koala + 10% discipline + 20% nerd + 3% failing fast + 7% coloring outside the lines">🐨<sup>.60</sup> + 🥋<sup>.10</sup> + 🤓<sup>.20</sup> + 🤦‍<sup>.03</sup> + 🤪‍<sup>.07</sup></h1>
         
-        <section class="index-introduction_paragraph-content" aria-label="Spencer Bramson's introduction">
-          <p class="index-introduction_paragraph">Award-winning marketing executive specialized in consumer technology applications and packaged goods. Past clientele including, but not limited to;</p>
-          
-          <ul class="index-highlighted_clients"><li class="index-highlighed_client" v-for="indexClientLink in indexClientLinks"><a class="index-hightlighted_client-link" :href="indexClientLink.linkURL">{{ indexClientLink.linkTitle }}</a></li></ul>
-        </section>
+        <section class="index-paragraph-area" aria-label="Spencer Bramson's introduction"><p class="index-introduction_paragraph">Award-winning marketing executive specialized in consumer technology applications and packaged goods. Past clientele including, but not limited to;<ul class="index-highlighted_clients"><li class="index-highlighed_client" v-for="indexClientLink in indexClientLinks"><a class="index-hightlighted_client-link" :href="indexClientLink.linkURL">{{ indexClientLink.linkTitle }}</a></li></ul></p></section>
         
       </section>
 
